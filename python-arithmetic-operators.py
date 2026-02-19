2d63e88e5358b8854e4d1595a8896635761f1c8c
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2026-02-19 17:21:03 +0300

    Implement basic arithmetic operations in Python

---
+++ python-arithmetic-operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum_ = a+b
+    substr = a-b
+    multi = a*b
+    
+    print(sum_)
+    print(substr)
+    print(multi)
