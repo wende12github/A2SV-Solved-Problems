44793fb6a6fbe0ca3da7eed6c93414dcf1b69467
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2026-03-06 09:32:41 +0300

    Implement countPairs method for divisible pairs

---
+++ count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        count = 0
+
+        for i in range(len(nums)-1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    product = i*j
+                    if product % k == 0:
+                        count += 1
+        return count
