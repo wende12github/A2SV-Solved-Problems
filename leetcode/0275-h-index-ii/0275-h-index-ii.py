a3562b7b26109fbfb00264da365db10386bce78d
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2026-04-02 17:12:06 +0300

    Add solution for H-Index II

--- leetcode/0275-h-index-ii/0275-h-index-ii.py
+++ leetcode/0275-h-index-ii/0275-h-index-ii.py
@@ -5,7 +5,7 @@ class Solution:
         left, right = 0, n-1
         result = 0
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (right + left) // 2
 
             if citations[mid] < n - mid:
                 left = mid + 1
