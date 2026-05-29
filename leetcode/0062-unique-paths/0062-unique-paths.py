38a8734f81b22fec8082a9b2f1764c23c4e212b6
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2026-05-29 09:50:47 +0300

    Add solution for Unique Paths

--- leetcode/0062-unique-paths/0062-unique-paths.py
+++ leetcode/0062-unique-paths/0062-unique-paths.py
@@ -1,16 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        grid = [[0 for _ in range(m)] for _ in range(n)]
+        row = [1] * m
         
-        for i in range(n):
-            for j in range(m):
-                if i == 0:
-                    grid[0][j] = 1
-                elif j == 0:
-                    grid[i][j] = 1
-                else:
-                    up = grid[i - 1][j]
-                    left = grid[i][j - 1]
-                    grid[i][j] = up + left
-                    
-        return grid[n - 1][m - 1]
+        for i in range(1, n):
+            for j in range(1, m):
+                row[j] += row[j - 1]
+                
+        return row[-1]
